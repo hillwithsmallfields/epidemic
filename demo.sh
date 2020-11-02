10f71bc41a0241e9cb32eb7f4076703b235abb0f
John Sturdy <jcg.sturdy@gmail.com>
2020-11-02 21:28:40 +0000

    Add video production

--- demo.sh
+++ demo.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-./epidemic -s 2000 -o /tmp/out.csv $*
+./epidimages -s 2000 -o /tmp/out.csv $*
 gnuplot -p epidemic.gnuplot
+gnuplot -p graph-png.gnuplot
+ffmpeg -r 60 -f image2 -s 1024x1024 -i day-%03d.png -vcodec libx264 -crf 25  -pix_fmt yuv420p spread.mp4
